7f318c50c896623298ba56110a7d86c11c7ed4c0
JackTheHack17 <101522436+JackTheHack17@users.noreply.github.com>
2023-08-29 18:08:29 -0500

    Cleaning things up

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -4,34 +4,51 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 //import com.revrobotics.CANSparkMax;
 //import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriverConstants;
 
 public class Drive extends SubsystemBase {
     private  WPI_TalonFX leftMaster; 
     private  WPI_TalonFX rightMaster; 
     private  WPI_TalonFX leftSlave; 
     private  WPI_TalonFX rightSlave;   
-    private DifferentialDrive drive; 
-
+    private DifferentialDrive drive;  
+    private Encoder encoder = new Encoder(DriverConstants.encoderChannelA,DriverConstants.encoderChannelB);
+    private final double kDriveTicksToFeet = 1.0 / 128 * DriverConstants.kWheelDiameter * Math.PI / 12;  
+    public final double getFeetPos(){ 
+        return encoder.get() * kDriveTicksToFeet; 
+    }
     public Drive(){ 
-        leftMaster = new WPI_TalonFX(5); 
-        rightMaster = new WPI_TalonFX(6);  
-        leftSlave = new WPI_TalonFX(7); 
-        rightSlave = new WPI_TalonFX(8);  
+        leftMaster = new WPI_TalonFX(DriverConstants.leftFront); 
+        rightMaster = new WPI_TalonFX(DriverConstants.rightFront);  
+        leftSlave = new WPI_TalonFX(DriverConstants.leftBack); 
+        rightSlave = new WPI_TalonFX(DriverConstants.rightBack);  
+        
+        leftSlave.follow(leftMaster); 
+        rightSlave.follow(rightMaster);   
 
         leftMaster.setInverted(true); 
         leftSlave.setInverted(true); 
 
-        leftSlave.follow(leftMaster); 
-        rightSlave.follow(rightMaster);  
+        
 
-        drive = new DifferentialDrive(leftMaster, rightMaster); 
+        drive = new DifferentialDrive(leftMaster, rightMaster);  
+        
 
+    }  
+
+    @Override 
+    public void periodic(){ 
+        SmartDashboard.putNumber("Drive encoder position:",getFeetPos());
     } 
+ 
+    
 
     public void arcadeDrive(double speed, double rotation){ 
-        drive.arcadeDrive(-speed,rotation);
+        drive.arcadeDrive(speed,rotation);
     }
 
 
